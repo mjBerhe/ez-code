2a41d93a45da279f320e6e72b86788ba5ae5ae7a
mjBerhe <mjberhe123@hotmail.com>
2024-07-18 12:37:54 -0400

    api working on server

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    GOOGLE_API_KEY: z.string(),
   },
 
   /**
@@ -17,6 +18,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    // NEXT_PUBLIC_GOOGLE_API_KEY: z.string(),
   },
 
   /**
@@ -25,6 +27,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
